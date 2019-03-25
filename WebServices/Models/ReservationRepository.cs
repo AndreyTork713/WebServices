7ae311d4bb8ceb908b6ce2faa83b3462bd2b9785
Andrey <tork713@gmail.com>
2019-03-26 00:09:28 +0700

    1

---
+++ WebServices/Models/ReservationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServices.Models
+{
+    public class ReservationRepository
+    {
+        private static ReservationRepository repo = new ReservationRepository();
+
+        public static ReservationRepository Current
+        {
+            get{return repo;}
+           
+        }
+
+        private List<Reservation> data = new List<Reservation>
+        {
+            new Reservation {ReservationID = 1, ClientName = "Петр", Location = "Отель"},
+            new Reservation {ReservationID = 1, ClientName = "Петр", Location = "Отель"},
+            new Reservation {ReservationID = 1, ClientName = "Петр", Location = "Отель"},
+
+        };
+
+    }
+}
